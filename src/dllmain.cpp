4538b51e99ed0964daea683f91c074c705b8bc54
sorozaza46-web <sorozaza46@gmail.com>
2026-06-28 15:17:24 +0300

    Refactor MainThread for improved readability and structure

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,76 +1,64 @@
 #include <Windows.h>
 #include "Packetlogger.h"
-#include "PacketManager.h"
 #include "BotManager.h"
-#include "SafeQueue.h"
+#include "PacketManager.h"
 #include "WinGui.h"
+
+BotManager botManager;
+SafeQueue qRecv;
 
 DWORD WINAPI MainThread(LPVOID param)
 {
-    HINSTANCE hInst = (HINSTANCE)param;
-    BotManager botManager;
-    SafeQueue qRecv;
+    // Arayüz Sınıfı Kaydı
+    WinGui::RegisterWindowClass((HINSTANCE)param);
 
+    // Motorları Başlat
     PacketManager::Initialize(&botManager);
     Packetlogger::Initialize(&qRecv);
     Packetlogger::HookRecv();
 
-    WinGui::RegisterWindowClass(hInst);
-    WinGui::CreateGuiWindow(hInst, &botManager, &qRecv);
+    // Görsel Arayüz Penceresini Aç
+    WinGui::CreateGuiWindow((HINSTANCE)param, &botManager, &qRecv);
 
-    MSG msg;
-    while (true)
+    // Ana Döngü (F12 tuşuna basılana kadar hileyi canlı tutar)
+    while (!GetAsyncKeyState(VK_F12))
     {
-        // 1. GUI Event Döngüsü
-        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+        // Gelen kuyruktaki paketleri işle
+        while (!qRecv.empty())
+        {
+            std::string packet = qRecv.front();
+            PacketManager::HandlePacket(packet);
+            qRecv.pop();
+        }
+
+        // Arka planda pencerelerin durumunu tetikle (Mesaj döngüsü pompalama)
+        MSG msg;
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
 
-        // 2. Ağ Paket İşleme Döngüsü
-        while (!qRecv.empty()) {
-            char* packet = qRecv.front();
-            if (packet) {
-                PacketManager::HandlePacket(std::string(packet));
-            }
-            qRecv.pop();
-        }
-
-        // Hotkey Kontrolleri
-        if (GetAsyncKeyState(VK_F5) & 1) {
-            if (!botManager.isRunning()) {
-                botManager.start();
-                botManager.startFishing();
-            }
-        }
-        if (GetAsyncKeyState(VK_F8) & 1) {
-            if (botManager.isRunning()) {
-                botManager.stop();
-            }
-        }
-        if (GetAsyncKeyState(VK_F12) & 1) {
-            break; // Döngüden çık, hileyi bellekten güvenle kaldır
-        }
+        // Menüyü asenkron güncellemek için sinyal gönder
+        SendMessage(WinGui::GetWindowHandle(), WM_USER + 100, 0, 0);
 
         Sleep(10);
     }
 
+    // Kapatma/Temizleme Aşaması (Unhook)
     Packetlogger::UnhookRecv();
-    if (WinGui::GetWindowHandle()) {
-        DestroyWindow(WinGui::GetWindowHandle());
-    }
+    DestroyWindow(WinGui::GetWindowHandle());
     
-    FreeLibraryAndExitThread(hInst, 0);
+    Sleep(100);
+    FreeLibraryAndExitThread((HMODULE)param, 0);
     return 0;
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
-    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+    {
         DisableThreadLibraryCalls(hModule);
-        // Gizlilik için Thread parametresi ile güvenli çalıştırma
-        CreateThread(NULL, 0, MainThread, hModule, 0, NULL);
+        CreateThread(0, 0, MainThread, hModule, 0, 0);
     }
     return TRUE;
 }
-
