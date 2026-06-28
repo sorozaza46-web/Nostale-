39780704d204a1c37daa499c3a1a812ff1983950
sorozaza46-web <sorozaza46@gmail.com>
2026-06-28 15:10:50 +0300

    Add FindPatternByString function to Memory namespace

--- src/Memory.h
+++ src/Memory.h
@@ -4,7 +4,7 @@
 namespace Memory
 {
     DWORD FindPattern(char* pattern, char* mask);
+    DWORD FindPatternByString(const char* szString); // Yeni: Akıllı dinamik arayıcı
     bool Hook(void* toHook, void* ourFunc, int len);
     void Patch(BYTE* dst, BYTE* src, unsigned int size);
 }
-
